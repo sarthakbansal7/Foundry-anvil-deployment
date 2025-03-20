f607a03b3e87dd654539f525e05225bdf9588acc
Sarthak <sarthakbansalog@gmail.com>
2025-03-20 22:28:49 +0530

    Add tests

---
+++ test/SimpleStorageTest.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import {DeploySimpleStorage} from "../script/DeploySimpleStorage.s.sol";
+import {SimpleStorage} from "../src/SimpleStorage.sol";
+import {Test} from "forge-std/Test.sol";
+
+contract SimpleStorageTest is Test {
+    
+}
